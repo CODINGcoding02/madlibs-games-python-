f2ab54d2c45644f5c7add81c133725470f6330ba
CODINGcoding02 <96243778+CODINGcoding02@users.noreply.github.com>
2021-12-16 21:57:55 +0530

    Create MyFile.py

---
+++ MyFile.py
@@ -0,0 +1,9 @@
+a=input("Enter the food:")
+b=input("Enter the name:")
+c=input("Enter the adjective:")
+d=input("Enter the noun:")
+e=input("Enter the verb1:")
+f=input("Enter the verb2:")
+g=input("Enter the verb3:")
+my_story="It was " +a +" day at the school,and "+b+" was super "+c+" for lunch.But when she went outside to eat,a "+d+" stole her "+a+" ! " +b +" chased the " +d+ " all over school.She " +e+" " +f+" and "+g+" througt the playgrounds.Then she tripped on the "+d+" and the " +d+ " cescaped!Luckily," +b+ " 's friends were willing to share thier "+a+" with her."
+print(my_story)
